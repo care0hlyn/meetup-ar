4d536fe2b76b9d8b7a9b0582d23f34276701328f
Carolyn Huynh <ckhuynh20@gmail.com>
2014-08-22 10:34:15 -0700

    create event table/schema

---
+++ db/migrate/20140822173156_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+    	t.column :description, :string
+    	t.column :location, :string
+    	t.column :start, :datetime
+    	t.column :end, :datetime
+    	t.timestamps
+    end
+  end
+end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,1 +1,5 @@
 require 'spec_helper'
+
+descrbe Event do
+	it { should validate }
+end
