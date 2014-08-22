b0caf36ce609e4a157305a35bcd3457f822d4628
Carolyn Huynh <ckhuynh20@gmail.com>
2014-08-22 14:14:38 -0700

    Create capitalization callback method for User class

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+	validates :name, :presence => true
 	has_many :events
+
+	before_save :capitalize
+
+	def capitalize
+		self.name = self.name.capitalize
+	end
 end
